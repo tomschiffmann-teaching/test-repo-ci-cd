bbb5e0fcc80be4276aa5e7e2bf5c607cd14851bd
Tom Schiffmann <tom.schiffmann@it-accelerator.com>
2026-03-25 18:17:59 +0100

    update values

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
 
   backend "s3" {
-    bucket = "amzn-bucket-bs"
+    bucket = "amzn-s3-bs-2026"
     key    = "app/terraform.tfstate" # Key könnt ihr selbst bestimmen, sollte nur über die deployments einheitlich sein
     region = "us-east-1"
   }
@@ -40,7 +40,7 @@ resource "aws_security_group" "ssh" {
 resource "aws_instance" "demo" {
   ami                    = "ami-0b6c6ebed2801a5cb" # ubuntu
   instance_type          = "t2.micro"
-  key_name               = "third-key-pair"
+  key_name               = "aws-key-pai"
   vpc_security_group_ids = [aws_security_group.ssh.id]
   tags = {
     Name = "test-server-iac"
